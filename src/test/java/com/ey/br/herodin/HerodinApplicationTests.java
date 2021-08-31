9a256687e015c9c3bfe6193f151c8f3daaf00948
Rinaldo Reverte Mendes Junior <rinaldojunior2012@gmail.com>
2021-08-31 09:08:12 -0400

    Update junit tests

--- src/test/java/com/ey/br/herodin/HerodinApplicationTests.java
+++ src/test/java/com/ey/br/herodin/HerodinApplicationTests.java
@@ -1,13 +1,33 @@
 package com.ey.br.herodin;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HerodinApplicationTests {
 
-    @Test
-    void contextLoads() {
+	@Test
+    public void validatorCreate() {
+    	boolean result = checkNomeHero("Miranha");    	
+    	assertTrue(result);
     }
+    
+	@Test
+    public void invalidatorCreate() {
+    	boolean result = checkNomeHero("Mir4nh4");    	
+    	assertFalse(result);
+    }
+
+	private boolean checkNomeHero(String string) {
+		if(string.matches("[a-zA-Z]+")){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 
 }
